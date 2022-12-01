fcd660b636156bbb4aaccde933856db9daab2dcc
RajaSekharReddyMadire <111790807+RajaSekharReddyMadire@users.noreply.github.com>
2022-11-30 20:59:19 -0600

    added the function succesfully

--- models/dogs.js
+++ models/dogs.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({ 
  dogName : { type : String, required : true },
  dogType : { type : String, required : true }, 
- price : { type : Number, required : true}
+ price : { type : Number, required : true, min: 1}  
 }) 
  
 module.exports = mongoose.model("Dog", dogSchema) 
